8926c1e70c3da6a731d8d1cb0b35a72f8652ef8a
Vinicius Okamoto <leo_kamoto@hotmail.com>
2020-08-16 20:00:07 -0300

    feat(constants): added colors

---
+++ lib/core/values/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+const Color primaryColor = blueLightColor;
+const Color blueLightColor = Color(0xff4D9CDA);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color blackColor = Color(0xff000000);
