328fbdb62044e231cd85eff9b34e2938462de801
Vinicius Okamoto <leo_kamoto@hotmail.com>
2020-08-17 15:10:59 -0300

    feat(localization): added translations

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'dart:io';
+
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:trzapp/core/di/service_locator.dart';
 import 'package:trzapp/core/trz_app.dart';
 
 void main() async {
   await setupLocator();
-  runApp(TRZApp());
+  while (Platform.localeName == null) {
+    await Future.delayed(const Duration(microseconds: 300), () {});
+  }
+  runApp(
+    EasyLocalization(
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('pt', 'BR'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: Locale('pt', 'BR'),
+      child: TRZApp(),
+    ),
+  );
 }
