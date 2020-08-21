aabf6c79ac000caf1dc4018ee5db167f8edc70fe
Vinicius Okamoto <leo_kamoto@hotmail.com>
2020-08-21 08:12:17 -0300

    feat(contact): added report

--- lib/features/main/apresentation/stores/main/views/contact/contact_store.dart
+++ lib/features/main/apresentation/stores/main/views/contact/contact_store.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:mobx/mobx.dart';
 import 'package:trzapp/core/di/service_locator.dart';
 import 'package:trzapp/features/main/domain/usecases/contact/manage_contact.dart';
 import 'package:trzapp/features/shared/data/mappers/user_mapper.dart';
 import 'package:trzapp/features/shared/domain/entities/user.dart';
+import 'package:trzapp/features/shared/domain/usecases/people/report_user.dart';
 
 part 'contact_store.g.dart';
 
@@ -12,7 +14,7 @@ class ContactStore = _ContactStoreBase with _$ContactStore;
 
 abstract class _ContactStoreBase with Store {
   final IManageContact _contact = serviceLocator<IManageContact>();
-
+  final IReportUser _reportUser = serviceLocator<IReportUser>();
   @observable
   User user = User();
 
@@ -39,9 +41,26 @@ abstract class _ContactStoreBase with Store {
         );
   }
 
-  addContact(String user) async {
+  @action
+  Future<String> reportUser(String id, String infected) async {
+    Response response = await _reportUser.reportUser(id, infected);
+    if (response.statusCode == 204 || response.statusCode == 404) {
+      await _contact.deleteContact(infected);
+      listUsers.removeWhere((element) => element.id == infected);
+      return "OK";
+    }
+    if (response.statusCode == 422) {
+      return "PAGES.MAIN.CONTACT.REPORT.ALREADY_REPORTED";
+    }
+
+    return "ERROR.ERROR_UNEXPECTED";
+  }
+
+  addContact(String user, String id) async {
     Map<String, dynamic> map = json.decode(user);
-    print(map);
-    await _contact.saveContact(UserMapper.fromJson(map));
+    if (map["id"] != id) {
+      await _contact.saveContact(UserMapper.fromJson(map));
+      await getAllContacts();
+    }
   }
 }
